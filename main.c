78d4b0fa7df9141d5b3e782869abf378c80b592f
marcio rodriguez <marcinhoperez10@gmail.com>
2026-08-30 09:37:38 -0300

    implementacao das funcoes static

--- main.c
+++ main.c
@@ -14,3 +14,12 @@ static int parse_positive_int(const char *str, int *out){
     *out = (int)val;
     return 0;
 }
+
+static double elapsed_seconds(struct timespec start, struct timespec end){
+    return (double)(end.tv_sec - start.tv_sec) + (double)(end.tv_nsec - start.tv_nsec) / 1e9;
+}
+
+static void free_all(int *a, int *b, int *c, int *d){
+    free(a); free(b); free(c); free(d);
+}
+
