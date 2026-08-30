6aebc8bdc507376b7a33037b350442ca23bfcc71
marcio rodriguez <marcinhoperez10@gmail.com>
2026-08-30 16:30:17 -0300

    finalizacao threads 2

--- pthreads2.c
+++ pthreads2.c
@@ -36,3 +36,45 @@ static void *worker2(void *arg){
     }
     return NULL;
 }
+
+int compute_pthreads2(const Config *cfg, int *image){
+    int n = cfg -> num_threads;
+    pthread_t *threads = malloc(n * sizeof(pthread_t));
+    ThreadsArgs2 * targs = malloc(n * sizeof(ThreadsArgs2));
+    int next_row = 0;
+    pthread_mutex_t mutex;
+
+    if(threads == NULL || targs == NULL){
+        fprintf(stderr, "Erro!! Falha ao alocar memoria para threads (pthreads2)\n");
+        free(threads);
+        free(targs);
+        return -1;
+    }
+
+    pthread_mutex_init(&mutex, NULL);
+
+    for(int t = 0; t < n; t++){
+        targs[t].cfg = cfg;
+        targs[t].image = image;
+        targs[t].next_row = &next_row;
+        targs[t].mutex = &mutex;
+
+        if(pthread_create(&threads[t], NULL, worker2, &targs[t]) != 0){
+            fprintf(stderr, "Erro!! Falha ao criar thread %d (pthreads2)\n", t);
+            for(int j = 0; j < t; j++) pthread_join(threads[j], NULL);
+            pthread_mutex_destroy(&mutex);
+            free(threads);
+            free(targs);
+            return -1;
+        }
+    }
+
+    for(int t = 0; t < n; t++){
+        pthread_join(threads[t], NULL);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    free(threads);
+    free(targs);
+    return 0;
+}
