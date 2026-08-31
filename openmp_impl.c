b76814d4ba133dff4d23ee40431ab5fd37aa2bb8
marcio rodriguez <marcinhoperez10@gmail.com>
2026-08-31 15:58:41 -0300

    criacao openmp.c

---
+++ openmp_impl.c
@@ -0,0 +1,15 @@
+#include "mandelbrot.h"
+
+void compute_openmp(const Config *cfg, int *image){
+    double re_step = (RE_MAX - RE_MIN) / cfg -> width;
+    double im_step = (IM_MAX - IM_MIN) / cfg -> height;
+
+    #pragma omp parallel for num_threads(cfg -> num_threads) schedule(static)
+    for(int y = 0; y < cfg -> height; y++){
+        double c_imag = IM_MIN + y * im_step;
+        for(int x = 0; x < cfg -> width; x++){
+            double c_real = RE_MIN + x * re_step;
+            image[y * cfg -> width + x] = mandelbrot_iterations(c_real, c_imag, cfg -> max_iter);
+        }
+    }
+}
